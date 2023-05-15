b58f6841eb05ae08e45915f2fe788f5f5db3466b
Fredrik <fredrik01@hotmail.se>
2023-05-15 12:11:01 +0200

    Added the two buttons

--- app/src/main/java/com/example/matvinn/MainActivity.kt
+++ app/src/main/java/com/example/matvinn/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.matvinn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +9,26 @@ import android.widget.Button
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
-    private lateinit var button2: Button
+    //private lateinit var button: Button
+    //private lateinit var button2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOpenNewActivity = findViewById<Button>(R.id.button)
+        buttonOpenNewActivity.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        val buttonOpenNewActivity2 = findViewById<Button>(R.id.button2)
+        buttonOpenNewActivity2.setOnClickListener{
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+
+        /*
         button = findViewById(R.id.button)
         button2 = findViewById(R.id.button2)
 
@@ -21,6 +37,6 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Knapp tryckt")
             }
 
-        })
+        })*/
     }
 }
