d49c11987814570be08460829f6363fdb928e3e3
Hannisssss <Chat3y@gmail.com>
2023-05-17 16:40:00 +0200

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/matvinn/MainActivity3.kt
+++ app/src/main/java/com/example/matvinn/MainActivity3.kt
@@ -1,11 +1,19 @@
 package com.example.matvinn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val button5 = findViewById<Button>(R.id.button5)
+        button5.setOnClickListener {
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/matvinn/MainActivity4.kt
@@ -0,0 +1,14 @@
+package com.example.matvinn
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.matvinn.R
+
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+
+    }
+}
